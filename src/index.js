c0d7a0e237f9ed76ceba23d053fc7d7bc2efc4d5
aliaksandraRybak <aliaksandra.rybak@gmail.com>
2018-09-15 18:54:13 +0300

    task 3 sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,57 @@
 class Sorter {
   constructor() {
-    // your implementation
+    return this;
   }
 
-  add(element) {
-    // your implementation
+ add(element) {
+    if (this.arr === undefined) {
+      this.arr = [];
+    } 
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices) { 
+    this.defaultComparator = function compareNum(a, b) { 
+      return a - b; 
+    }; 
+    indices.sort(this.defaultComparator); 
+
+    if (this.comparator === undefined) { 
+      this.comparator = this.defaultComparator; 
+    } 
+
+    for (var i = 0; i < indices.length - 1; i++) { 
+      for (var j = i + 1; j < indices.length; j++) { 
+        var first = indices[i]; 
+        var second = indices[j]; 
+
+        if (this.comparator(this.arr[first], this.arr[second]) > 0) { 
+          swap(first, second, this.arr) 
+        } 
+      } 
+    }
+     
+    function swap(first, second, arr) { 
+      var temp = arr[first]; 
+      arr[first] = arr[second]; 
+      arr[second] = temp; 
+    } 
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    return this.comparator = compareFunction;
   }
 }
 
